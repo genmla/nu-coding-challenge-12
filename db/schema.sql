5f9d389b66b5e490296d9a6c1e93678ff2c01853
genmla <gmmladenka@gmail.com>
2022-12-17 13:28:29 -0600

    update seeds and schema for tables and data without error

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE company_db;
 
 CREATE TABLE departments (
   id INT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE roles (
@@ -14,8 +14,7 @@ CREATE TABLE roles (
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
+    REFERENCES departments(id)  
 );
 
 CREATE TABLE employees (
@@ -24,10 +23,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
-    ON DELETE SET NULL, 
-    manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employees(id)
-    ON DELETE SET NULL
-);
+    REFERENCES roles(id), 
+    manager_id INT REFERENCES employees(id)
+    );
