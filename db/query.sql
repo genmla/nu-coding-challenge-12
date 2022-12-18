7b6640b214349e2a0748ba56a29525b3c1933d41
genmla <gmmladenka@gmail.com>
2022-12-18 10:30:11 -0600

    selected desired desired fields from employee and joined with roles and departments tables

--- db/query.sql
+++ db/query.sql
@@ -3,3 +3,8 @@ FROM reviews
 LEFT JOIN movies
 ON reviews.movie_id = movies.id
 ORDER BY movies.movie_name;
+
+SELECT employees.id, employees.first_name AS First, employees.last_name AS Last, roles.title AS Title, roles.salary As Salary, departments.name AS Department 
+FROM employees
+INNER JOIN roles ON roles.id = employees.role_id
+INNER JOIN departments ON departments.id = roles.department_id;
