e64a0bd46e492a94eef542c574df588baa316a8f
genmla <gmmladenka@gmail.com>
2022-12-19 17:55:37 -0600

    add view to view employees by department

--- db/query.sql
+++ db/query.sql
@@ -26,3 +26,7 @@ SELECT employees.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, CONCAT(
 SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, CONCAT(m.first_name, ' ', m.last_name) AS Manager, roles.title AS Title, roles.salary As Salary, departments.name AS Department FROM employees e LEFT JOIN employees m ON m.id = e.manager_id INNER JOIN roles ON roles.id = e.role_id INNER JOIN departments ON departments.id = roles.department_id;
 
 CREATE OR REPLACE VIEW ManagerView AS SELECT CONCAT(first_name, " ", last_name) AS Employees FROM employees WHERE manager_id = 2; SELECT * FROM ManagerView
+
+CREATE OR REPLACE VIEW DeptView AS SELECT employees.id, CONCAT(employees.first_name, ' ', employees.last_name) AS Employee, roles.title AS Title, roles.salary As Salary, FROM employees INNER JOIN roles ON roles.id = employees.role_id INNER JOIN departments ON departments.id = roles.department_id WHERE departments.id = 1;
+
+CREATE OR REPLACE VIEW DeptView AS SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, CONCAT(m.first_name, ' ', m.last_name) AS Manager, roles.title AS Title, roles.salary As Salary, departments.name AS Department FROM employees e LEFT JOIN employees m ON m.id = e.manager_id INNER JOIN roles ON roles.id = e.role_id INNER JOIN departments ON departments.id = roles.department_id WHERE departments.id = 1
